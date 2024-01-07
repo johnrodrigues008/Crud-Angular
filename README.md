3d6d23ef6064cff5ac7f13fd3d8e921a1d2238cc
John Rodrigues <john.rodrigues008@gmail.com>
2024-01-06 23:36:05 -0300

    Criação dos componentes de adição de produtos e listagem
    Signed-off-by: John Rodrigues <john.rodrigues008@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Crud-Angular-Node-Express-Typescript-Sequelize-Mysql
+# 🔥Crud Angular 
+
+Esse projeto tem como objetivo demonstrar algumas funcionalidades do angular em uma arquitetura simples contendo o front e o backend de mandeira bem estruturada e orgnizada.
+
+### Tecnologias que iremos utilizar ao longo do projeto: 
+
+> 🪹 Frontend
+
+* HTML
+* CSS/SCSS
+* JavaScript/TypeScript
+* NodeJS
+* Angular JS
+* Bootswatch
+
+> 🧟 Backend:
+
+* TypeScript
+* Sequelize
+* Express
+
+> 🎁 Banco de dados:
+
+* Mysql
+* Docker
+* DBeaver || Xampp
--- frontend/src/app/components/list-products/list-products.component.ts
+++ frontend/src/app/components/list-products/list-products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-list-products',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-products.component.scss']
 })
 export class ListProductsComponent {
-
+  listProducts: Product[] = [
+    {   id: 1, name: 'Coca cola', description: 'Refrigerante com muito açucar', price: 22.50,stock: 500 },
+    {   id: 2, name: 'Wopper duplo', description: 'Hamburguer duplo com queijo, cebola, 2 carnes e salada', price: 38.90,stock: 10 },
+    {   id: 3, name: 'Batata frita', description: 'Batata fritas saborosas', price: 13.40 ,stock: 10 },
+  ]
 }
